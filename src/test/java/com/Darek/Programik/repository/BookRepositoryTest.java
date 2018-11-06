2c791f58742ac6c60d7a65607e622df2616b22ce
DariuszWawryszuk <44153798+DariuszWawryszuk@users.noreply.github.com>
2018-11-06 10:50:12 +0100

    mockito

--- src/test/java/com/Darek/Programik/repository/BookRepositoryTest.java
+++ src/test/java/com/Darek/Programik/repository/BookRepositoryTest.java
@@ -8,7 +8,7 @@ public class BookRepositoryTest {
     @Autowired
     BookRepository bookRepository;
 
-    @Test
+    
 
 
 }
--- src/test/java/com/Darek/Programik/service/BookServiceTest.java
+++ src/test/java/com/Darek/Programik/service/BookServiceTest.java
@@ -1,16 +1,34 @@
 package com.Darek.Programik.service;
 
+import com.Darek.Programik.model.BookEntity;
+import com.Darek.Programik.repository.BookRepository;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookServiceTest {
 
+    @InjectMocks
+    BookService bookServvice;
 
-    @Autowired
-    BookService bookService;
+    @Mock
+    private BookRepository bookRepository;
+
+    private ArrayList<BookEntity> Lista;
 
     @Test
-    void testSaveBook(){
+    public void testFindAllBooks() {
 
+        BDDMockito.given(bookRepository.findAllBooks()).willReturn(Lista);
+
+       List<BookEntity> books = bookServvice.findAllBooks();
+
+        Mockito.verify(bookRepository).findAllBooks();
     }
 }
